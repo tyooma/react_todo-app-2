b4cf1a8bfeabeb3ae6a8d9cde8e4babde4f26807
tyooma <flying.alien.godzilla@gmail.com>
2020-07-16 03:17:56 +0300

    Updates

---
+++ precache-manifest.f4c825257633a89f76b326edb4320832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23138946f33821436a121ca63dd32605",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0aa3f2f15b9b42b23396",
+    "url": "./static/css/main.73b98037.chunk.css"
+  },
+  {
+    "revision": "98517ed879938fe558da",
+    "url": "./static/js/2.cc334150.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cc334150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa3f2f15b9b42b23396",
+    "url": "./static/js/main.8d1b16e9.chunk.js"
+  },
+  {
+    "revision": "0a7feb5fb482d3bafdcc",
+    "url": "./static/js/runtime-main.5087bab6.js"
+  }
+]);
